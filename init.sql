58e88ef8f8e6929df77e723d75f979e1f934135e
mio2 <motty.mio2@gmail.com>
2022-03-11 02:35:52 +0000

    initialize for db

---
+++ init.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO `user` SET `name`="ほのか",`token`="RKMxBGuK", `leader_card_id`=42;
+-- INSERT INTO `user` SET `name`="うみ",`token`="sea", `leader_card_id`=43;
+
+SELECT `id`, `name`, `token`, `leader_card_id` FROM `user`;
+
+-- INSERT INFO `user` (`name`, `token`, `leader_card_id`) VALUES ("いぬ", "dog", 11), ("さる", "monkey", 44);
