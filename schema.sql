0a4f561210378ca9150edc2bd4790d2f64b1560f
mio2 <motty.mio2@gmail.com>
2022-03-14 02:44:09 +0000

    update sql settings

--- schema.sql
+++ schema.sql
@@ -7,3 +7,23 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
 );
+
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE `room` (
+  `room_id` bigint NOT NULL AUTO_INCREMENT,
+  `live_id` varchar(255) DEFAULT NULL,
+  `room_members_count` int DEFAULT NULL,
+  `max_user_count` int DEFAULT 4,
+  `owner_id` bigint DEFAULT NULL,
+  `status` int DEFAULT 1,
+  PRIMARY KEY (`room_id`)
+);
+
+DROP TABLE IF EXISTS `room_member`;
+CREATE TABLE `room_member` (
+  `room_id` bigint NOT NULL,
+  `live_id` varchar(255) DEFAULT NULL,
+  `id` bigint DEFAULT NULL,
+  `exist` int DEFAULT 1,
+  PRIMARY KEY (`room_id`)
+);
